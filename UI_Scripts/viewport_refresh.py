23d7c256348aef37bee8c0a2579234d0d2cadcb5
zxcvvvvbanan <xz4540x@gmail.com>
2024-08-24 09:44:46 +0900

    init

---
+++ UI_Scripts/viewport_refresh.py
@@ -0,0 +1,53 @@
+def main():
+     desktop = hou.ui.curDesktop()
+     scene_viewer = desktop.paneTabOfType(hou.paneTabType.SceneViewer)
+     pane = scene_viewer.pane()
+ 
+     #save current viewport setting
+     oldscene = scene_viewer
+ 
+     #save curViewport camera object
+     cam = oldscene.curViewport().defaultCamera()
+     saved = cam.stash()
+ 
+     #save flipbook setting
+     startF,endF = oldscene.flipbookSettings().frameRange()
+     use_res = oldscene.flipbookSettings().useResolution()
+ 
+     #create new scene viewer
+     scene_viewer = pane.createTab(hou.paneTabType.SceneViewer)
+     newscene = scene_viewer
+ 
+     # start restoring flipbook settings to new scene viewer
+     # If you want to customize it,
+     # ask me or see hou.GeometryViewport class, hou.GeometryViewportCamera class to find methods you need
+ 
+     newscene.flipbookSettings().frameRange((startF,endF))
+     newscene.flipbookSettings().useResolution(use_res)
+ 
+     #restore toolbar states
+     newscene.setCurrentState(oldscene.currentState())
+     newscene.showOperationBar(oldscene.isShowingOperationBar())
+     newscene.showSelectionBar(oldscene.isShowingSelectionBar())
+     newscene.showDisplayOptionsBar(oldscene.isShowingDisplayOptionsBar())
+ 
+     #restore scene viewer setting
+     newscene.setSnappingMode(oldscene.snappingMode())
+     newscene.setPickingVisibleGeometry(oldscene.isPickingVisibleGeometry())
+     newscene.setPickingContainedGeometry(oldscene.isPickingContainedGeometry())
+     newscene.setWholeGeometryPicking(oldscene.isWholeGeometryPicking())
+     newscene.setSecureSelection(oldscene.isSecureSelection())
+ 
+     #restore camera
+     #If current viewport has cam selected, it restores it's value
+     #if not, current camera data extracted above (cam.stash) will be restored.
+     if oldscene.curViewport().camera() is None :
+         newscene.curViewport().setDefaultCamera(saved)
+     else :
+         newscene.curViewport().setCamera(oldscene.curViewport().camera())
+ 
+     #terminate old scene viewer
+     oldscene.close()
+ 
+ 
+main()
